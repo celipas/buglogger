0882dcc58c010c6dcf0d6045f82da01f29b0d708
celipas <celipas@gmail.com>
2020-11-11 20:49:37 -0700

    working on auth

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+
+// Define Routes
+
+// Serve static assets in product
